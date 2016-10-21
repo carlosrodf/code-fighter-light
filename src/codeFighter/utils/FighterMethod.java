055f11eebc917025ce6c44b13bb38902a978496e
Carlos Rodriguez <carlosrod.furlan@gmail.com>
2016-10-21 12:48:24 -0600

    test definition added to method class

--- src/codeFighter/utils/FighterMethod.java
+++ src/codeFighter/utils/FighterMethod.java
@@ -1,5 +1,8 @@
 package codeFighter.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author carlosrodf
@@ -11,11 +14,14 @@ public class FighterMethod {
     private Class<?>[] parameterTypes;
     private Object[] parameterValues;
     
+    private List<FighterTest> tests;
+    
     public FighterMethod(String methodCode, String methodName){
         this.methodCode = methodCode;
         this.methodName = methodName;
         this.parameterTypes = null;
         this.parameterValues = null;
+        this.tests = new ArrayList<>();
     }
     
     public boolean isValid(){
@@ -30,6 +36,22 @@ public class FighterMethod {
         }else{
             return false;
         }
+    }
+    
+    public void addTest(FighterTest test){
+        this.tests.add(test);
+    }
+    
+    public void addTest(Object[] parameterValues, Object expectedReturnValue,boolean hidden){
+        if(hidden){
+            this.tests.add(FighterTest.createHiddenTest(parameterValues, expectedReturnValue));
+        }else{
+            this.tests.add(FighterTest.createPublicTest(parameterValues, expectedReturnValue));
+        }
+    }
+    
+    public void removeAllTests(){
+        this.tests.clear();
     }
     
     public void setParameterTypes(Class<?>... parameterTypes){
--- src/codeFighter/utils/FighterTest.java
+++ src/codeFighter/utils/FighterTest.java
@@ -6,8 +6,35 @@ package codeFighter.utils;
  */
 public class FighterTest {
     
-    public FighterTest(){
-        
+    private final Object[] parameterValues;
+    private final Object expectedReturnValue;
+
+    private final boolean hidden;
+    
+    public static FighterTest createHiddenTest(Object[] parameterValues, Object expectedReturnValue){
+        return new FighterTest(parameterValues,expectedReturnValue,true);
+    }
+    
+    public static FighterTest createPublicTest(Object[] parameterValues, Object expectedReturnValue){
+        return new FighterTest(parameterValues,expectedReturnValue,false);
+    }
+
+    public Object[] getParameterValues() {
+        return parameterValues;
+    }
+    
+    public boolean testPassed(Object actualReturnValue){
+        return expectedReturnValue.equals(actualReturnValue);
+    }
+    
+    public boolean isHidden() {
+        return hidden;
+    }
+    
+    private FighterTest(Object[] parameterValues, Object expectedReturnValue,boolean hidden){
+        this.parameterValues = parameterValues;
+        this.expectedReturnValue = expectedReturnValue;
+        this.hidden = hidden;
     }
     
 }
